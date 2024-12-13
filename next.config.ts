303a683388e1dbd906a0e8c7a352286deeef1b34
Saiprasad Medishetti <saiprasadmedishetti@Saiprasads-MacBook-Air.local>
2024-12-13 20:53:14 +0530

    fix: icons

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.xe.com",
+        pathname: "svgs/flags",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
